521fc4ca8f989fee397dc666f86e2ff861e2f43e
Giovanni Lucia <56722995+natras2@users.noreply.github.com>
2024-08-20 11:01:16 +0200

    update system indentation

--- client-app/src/index.js
+++ client-app/src/index.js
@@ -3,24 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './assets/css/style.css';
 
-//import { initializeApp } from "firebase/app";
-
-// Your web app's Firebase configuration
-//const firebaseConfig = {
-//  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-//  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-//  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-//  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-//  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-//  appId: process.env.REACT_APP_FIREBASE_APP_ID
-//};
-
-// Initialize Firebase
-//const app = initializeApp(firebaseConfig);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
