2cc5bbd94dfb1e96ec260f9e18c9dd48ff029ae5
Giovanni Lucia <56722995+natras2@users.noreply.github.com>
2024-11-12 00:08:45 +0100

    Update endpoint.js

--- server/auth/endpoint.js
+++ server/auth/endpoint.js
@@ -5,7 +5,7 @@ const authRouter = express.Router();
 authRouter.post('/authenticate', async (req, res) => {
     try {
         await userAuthenticate(req.user);
-        res.status(200).json({ message: 'All clear. Here\'s a biscuit' });
+        res.status(200).json({ message: 'All clear. Delightful.' });
     } 
     catch(err) {
         res.status(500).json({ message: 'Error authenticating the user', error: err });
